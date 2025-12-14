f7e0218eb297e424f5328dd977a62abee2d9dd1a
stable-release <stable-release@users.noreply.github.com>
2025-12-15 01:03:21 +0800

    Hello world

--- tcp-echo-server/src/main.rs
+++ tcp-echo-server/src/main.rs
@@ -1,3 +1,27 @@
+use std::{io::Write, net::{TcpListener, TcpStream}};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(l) => l,
+        Err(e) => {
+            eprintln!("{}", e);
+            return ();
+        }
+    };
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = "hello".to_string();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap()
 }
